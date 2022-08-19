601e7f048c52b091f5745226d28abb1d9972d19d
parth191220 <47950994+parth191220@users.noreply.github.com>
2022-08-19 23:15:09 +0530

    Create Design_a_Trie.java

---
+++ Design_a_Trie.java
@@ -0,0 +1,80 @@
+class Trie {
+    
+    Node root; 
+
+    public Trie() {
+        root = new Node(); 
+    }
+    
+    public void insert(String word) {
+        Node current = root; 
+        
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i); 
+            
+            Node node = current.children.get(c); 
+            
+            if (node == null) {
+                node = new Node(); 
+                
+                current.children.put(c, node); 
+            }
+            
+            current = node; 
+        }
+        
+        current.endOfWord = true; 
+        
+    }
+    
+    public boolean search(String word) {
+        Node current = root; 
+        
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i); 
+            
+            Node node = current.children.get(c); 
+            
+            if (node == null) return false; 
+            
+            current = node; 
+        }
+        
+        return current.endOfWord; 
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node current = root; 
+        
+        for (int i = 0; i < prefix.length(); i++) {
+            Character c = prefix.charAt(i); 
+            
+            Node node = current.children.get(c); 
+            
+            if (node == null) return false; 
+            
+            current = node; 
+            
+        }
+        
+        return true; 
+    }
+}
+
+class Node {
+    Map<Character, Node> children;
+    boolean endOfWord; 
+    
+    Node() {
+        children = new HashMap<>(); 
+        endOfWord = false; 
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
